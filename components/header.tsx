f90eff12355fe5de7e51fab0af3843549463cd7d
johnelipse <johnelipse472@gmail.comgit config --global user.name johnelipseOmit --globalgit commit -meventgit config --global user.email johnelipse472@gmail.com>
2024-08-25 18:33:55 +0300

    frontend header modified

--- components/header.tsx
+++ components/header.tsx
@@ -58,7 +58,7 @@ const navigation =[
     },
 ]
   return (
-    <header className="sticky justify-between  z-[9999] top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 w-full">
+    <header className="sticky justify-between  z-[20] top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 w-full">
 
 <nav className="hidden  flex-col justify-between gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
       <Link
@@ -100,7 +100,7 @@ const navigation =[
         </Button>
       </SheetTrigger>
       <SheetContent side="left">
-        <nav className="grid gap-6 text-lg font-medium">
+        <nav className="grid gap-6 text-lg font-medium z-[30]">
           {
             smallnavigation.map((item, i)=>{
               return(
@@ -120,7 +120,7 @@ const navigation =[
     </Sheet>
 
     <div className="flex items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-      <Link href='/' className='bg-black px-3 py-1 rounded-md text-white'>Signup</Link>
+      <Link href='/' className='bg-black px-3 lg:block md:block hidden py-1 rounded-md text-white'>Signup</Link>
       <Link href='/' className='flex gap-3 items-center'>write <FolderPen className='w-4 h-4'/></Link>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
