decac77ba06349f61a888006bdf8256cfca5c77a
johnelipse <johnelipse472@gmail.comgit config --global user.name johnelipseOmit --globalgit commit -meventgit config --global user.email johnelipse472@gmail.com>
2024-08-30 16:58:21 +0300

    categories added2

--- types/types.ts
+++ types/types.ts
@@ -24,7 +24,7 @@ export type UserDetails = {
 export type CatProps = {
   title: string;
   slug: string;
-  image: string;
+  image: string ;
   singleCat: string | any
 };
 export type CatPropsUpdate = {
